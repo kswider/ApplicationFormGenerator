964747ba0f71efcf750bb6738fab7b470ac97c12
Krzysiek <krzyztof.swider@motorolasolutions.com>
2020-05-29 00:16:08 +0200

    Added ability to save forms in single file

--- ApplicationFormGenerator/Program.cs
+++ ApplicationFormGenerator/Program.cs
@@ -1,6 +1,8 @@
 ﻿using ApplicationFormGenerator.FormGenerators;
 using System;
+using System.Collections.Concurrent;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ApplicationFormGenerator
@@ -21,16 +23,39 @@ namespace ApplicationFormGenerator
             Console.WriteLine($"Provide staus of generated docs: 1 - Positive Cases, 2 - Negative Cases");
             bool shouldCreatePositiveCases = int.Parse(Console.ReadLine()) == 1;
 
+            Console.WriteLine($"SignleFileOutput: 1 - Yes, 2 - No");
+            bool singleFileOutput = int.Parse(Console.ReadLine()) == 1;
+
             var generator = GeneratorFactory.CreateGenerator(documentType);
             if (Directory.Exists(OutputDirectory))
                 Directory.Delete(OutputDirectory, true);
 
             Directory.CreateDirectory(OutputDirectory);
-            Parallel.For(0, numberOfDocsToGenerate, i =>
+            if (!singleFileOutput)
             {
-                var text = generator.Generate(shouldCreatePositiveCases);
-                File.WriteAllText($@"{OutputDirectory}/doc_{i}", text);
-            });
+                Parallel.For(0, numberOfDocsToGenerate, i =>
+                {
+                    var text = generator.Generate(shouldCreatePositiveCases);
+                    File.WriteAllText($@"{OutputDirectory}/doc_{i}", text);
+                });
+            }
+            else
+            {
+                ConcurrentBag<string> texts = new ConcurrentBag<string>();
+                Parallel.For(0, numberOfDocsToGenerate, i =>
+                {
+                    var text = generator.Generate(shouldCreatePositiveCases);
+                    texts.Add(text);
+                });
+                using (var sw = new StreamWriter($@"{OutputDirectory}/output"))
+                {
+                    foreach (var text in texts)
+                    {
+                        var textSpacesSeparated = Regex.Replace(text, @"\s+", " ");
+                        sw.WriteLine($"{shouldCreatePositiveCases};{textSpacesSeparated}");
+                    }
+                }
+            }
             Console.WriteLine("Files generated successfully!");
         }
     }
